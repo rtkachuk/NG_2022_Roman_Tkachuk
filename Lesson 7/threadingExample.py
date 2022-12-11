203c2d9647b66ead0f14776dc695782e6ec840e3
Roman Tkachuk <tkachukroman64@gmail.com>
2022-12-11 19:10:58 +0200

    Created lesson 7 for threads :3

---
+++ Lesson 7/threadingExample.py
@@ -0,0 +1,25 @@
+import threading
+
+def showLetters(number, word):
+    for letter in word:
+        print ("Thread #" + str(number) + ":" + letter)
+
+def manager(lines):
+    threads = []
+    for line in lines:
+        threads.append(threading.Thread(target=showLetters, args=(lines.index(line), line, )))
+
+    for thread in threads:
+        thread.start()
+
+    #for thread in threads:
+     #   thread.join()
+
+lines = [
+    "Hello world",
+    "This is an example line, which will be shown letter by letter",
+    "Well, this is awkward, but I perfer python for desktop applications",
+    "I hope, that chromium will be stable in future"
+]
+
+manager(lines)
---
+++ Lesson 7/threadsSyncExample.py
@@ -0,0 +1,26 @@
+import threading
+import time
+
+lock = threading.Lock()
+number = 0
+
+def incrementNumber(threadNumber):
+    global number
+    while number < 100:
+        with lock:
+            number = number + 1
+            print ("Thread #" + str(threadNumber) + ":" + str(number))
+        time.sleep(0.1)
+
+def manager():
+    threads = []
+    for threadNumber in range (0, 10):
+        threads.append(threading.Thread(target=incrementNumber, args=(threadNumber, )))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+manager()
